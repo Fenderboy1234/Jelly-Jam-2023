4873d0db6d53c588f2711d8059daf686300f2a58
Fenderboy1234 <36420192+Fenderboy1234@users.noreply.github.com>
2023-05-15 20:55:22 -0400

    SampleScene Changes Fenderboy

--- Assets/UIFadeText.cs
+++ Assets/UIFadeText.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class UIFadeText : MonoBehaviour
 {
+    float timer;
+    string text = "thing"
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= timer.deltaTime;
+        if (timer > 0)
+        {
+
+        }
     }
 }
